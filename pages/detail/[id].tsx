75b6bfe93a6e39ef53d83f1ac844090fd39698d3
A-Sal3 <asalinas3205@gmail.com>
2025-05-07 22:49:02 -0500

    YTK-42 Create function comment backend

--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -24,6 +24,8 @@ const Detail = ({ postDetails }: IProps) => {
   const [post, setPost] = useState(postDetails);
   const [playing, setPlaying] = useState(false);
   const [isVideoMuted, setIsVideoMuted] = useState(false);
+  const [comment, setComment] = useState('');
+  const [isPostingComment, setIsPostingComment] = useState(false);
   const videoRef = useRef<HTMLVideoElement>(null);
   const router = useRouter();
   const { userProfile }: any = useAuthStore();
@@ -55,6 +57,23 @@ const Detail = ({ postDetails }: IProps) => {
       });
 
       setPost({ ...post, likes: data.likes });
+    }
+  }
+
+  const addComment = async (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+
+    if(userProfile && comment) {
+      setIsPostingComment(true);
+
+      const res = await axios.put(`${BASE_URL}/api/post/${post._id}`, {
+        userId: userProfile._id,
+        comment,
+      });
+
+      setPost({ ...post, comments: res.data.comments });
+      setComment('');
+      setIsPostingComment(false);
     }
   }
 
@@ -137,7 +156,13 @@ const Detail = ({ postDetails }: IProps) => {
               />
             )}
           </div>
-          <Comments />
+          <Comments 
+            comment={comment}
+            setComment={setComment}
+            addComment={addComment}
+            comments={post.comments}
+            isPostingComment={isPostingComment}
+          />
         </div>
       </div>
     </div>
